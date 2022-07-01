30f0ca1885d79f53e19230e9ccd445edb485093b
iscanzures <iscanzures@gmail.com>
2022-07-01 17:09:22 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDTjv5MpG0mI5PCb9GTgVxroUuE4tFAE_M",
+
+  authDomain: "lgbtttiq-plus.firebaseapp.com",
+
+  databaseURL: "https://lgbtttiq-plus-default-rtdb.firebaseio.com",
+
+  projectId: "lgbtttiq-plus",
+
+  storageBucket: "lgbtttiq-plus.appspot.com",
+
+  messagingSenderId: "53474800706",
+
+  appId: "1:53474800706:web:392066e979854cdd4d69db"
+
+};
